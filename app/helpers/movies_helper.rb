f0393ccc2cf6b54db66e09446cc09a6a77b5150d
root <root@saasbook.(none)>
2012-03-27 20:16:51 -0700

    first commit

--- app/helpers/movies_helper.rb
+++
@@ -1,5 +0,0 @@
-module MoviesHelper
-  def oddness(count)
-    count.odd? ?  "odd" :  "even"
-  end
-end
--- spec/models/movie_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Movie do
-  describe 'calling TMDb' do
-    it 'should call the TMDb gem to do the search' do
-      Tmdb
-  end
-end

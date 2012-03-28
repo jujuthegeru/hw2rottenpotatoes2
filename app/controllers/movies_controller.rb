73b80256ac5ce5fb1deb42ab127c0b3f05e233af
jujuthergeru <germao216@hotmail.com>
2012-03-27 20:51:58 -0700

    Rottenpotatoes

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @movies = Movie.all
+    
+   
+    if params[:title_sort]
+
+      ordering,@title_header = {:order => :title}, 'hilite'
+
+    elsif params[:release_date_sort]
+
+      ordering,@date_header = {:order => :release_date}, 'hilite'
+
+    end
+
+    @all_ratings = Movie.all_ratings
+
+    @selected_ratings = (params[:ratings] ||= {}).keys
+
+    @movies = Movie.find_all_by_rating(@selected_ratings, ordering)
+
+  
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ActiveRecord::Base
+
+def self.all_ratings
+
+    %w(G PG PG-13 NC-17 R)
+
+  end
+end
